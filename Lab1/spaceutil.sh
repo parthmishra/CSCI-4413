7e9d9ea8b1735346fffe0942ea2b48be765bccfa
Parth Mishra <pmishraworld@mac.com>
2016-09-07 23:22:57 -0600

    Update spaceutil.sh

--- Lab1/spaceutil.sh
+++ Lab1/spaceutil.sh
@@ -5,5 +5,5 @@ TEST="$(df -h | head -2 | tail -1 | awk '{print $5}' | cut -d% -f1)"
 
 if [ $TEST -gt $UTIL ]
 then
-	echo "Disk Util is over 80%!" | mail -s "Disk Util Alert!" root	
+	echo "Disk Util is over 80%!" | mail -s "Disk Util Alert!" root@localhost	
 fi
